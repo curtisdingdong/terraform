89d8f6b6a4c483ecaa70898a0e53946dae398942
encryptos <thechase22@hotmail.com>
2025-01-30 20:41:58 +0000

    adding var file

--- argocd/vars.tf
+++ argocd/vars.tf
@@ -1,4 +1,16 @@
 variable "namespace" {
+    type = string
     default = "argocd"
 }
 
+variable "argocd_user"{
+ type = string
+}
+
+variable "argocd_password"{
+    type = string
+}
+
+variable "argocd_server"{
+    type = string
+}
